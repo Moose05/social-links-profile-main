c811bbf47967c7767f854a58f5203282b6ea0c4e
Moose05 <musavictormnanzana@gmail.com>
2025-04-13 13:13:16 +0200

    Fixed a bug where it was leading to wrong accounts

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded",function(){
         LinkedIn: "https://www.linkedin.com/in/musawenkosi-mnanzana-71349b159",
         GitHub: "https://github.com/Moose05",
         frontendmentor:"https://www.frontendmentor.io/profile/Moose05",
-        Instagram:"https://www.instagram.com/kepturedsnaps?igsh=OGhnMzVlaDZwcHZ6&ut-m_source=qr",
-        TikTok:"https://www.TikTok.com/@wenkosii?_t=ZM-8vUoTYd1xyf&_r=1"
+        Instagram:"https://www.instagram.com/keptured.snaps?igsh=OGhnMzVlaDZwcHZ6&ut-m_source=qr",
+        TikTok:"https://www.TikTok.com/@wenkosiii?_t=ZM-8vUoTYd1xyf&_r=1"
     };
 
     Object.keys(buttonlinks).forEach(function (id){
